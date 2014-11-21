19c5a983ec209b04a4e5d3859d9a9758d18b2f38
Maciej Kumorek <mkumorek@gmail.com>
2014-11-21 17:35:03 +0000

    Upload images to blob storage

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,7 +1,7 @@
 var express = require('express'),
   router = express.Router(),
   Article = require('../models/article'),
-  formidable = require('formidable'),
+  multiparty = require('multiparty'),
   azure = require('azure-storage'),
   uuid = require('node-uuid'),
   entityGen = azure.TableUtilities.entityGenerator;
@@ -23,25 +23,36 @@ router.get('/', function (req, res, next) {
 router.post('/upload', function (req, res) {
 
   var blobSvc = azure.createBlobService();
-  var form = new formidable.IncomingForm();
-  res.respond("OK");
+  var form = new multiparty.Form();
+  
+  // Handle form data
+  form.on('part', function(part){
+    if (part.filename) {
 
-  form.onPart = function(part){
-    blobSvc.createContainerIfNotExists('imagecontainer', function(error, result, response){
-      if(!error){
+      var size = part.byteCount - part.byteOffset;
+      var name = part.filename;
 
-        blobSvc.createBlockBlobFromStream('imagecontainer', 'image1', part, function(error, result, response){
-          if(!error){
-              // Blob uploaded
-          } else {
-            console.log(error);
-          }
-        });
-      } else {
-        console.log(error);
-      }
-    });
-  }
+      // See if container exists
+      blobSvc.createContainerIfNotExists('imagecontainer', function(error, result, response){
+        if(!error){
+
+          // If true, create blob
+          blobSvc.createBlockBlobFromStream('imagecontainer', name, part, size, function(error){
+            if(!error){
+                // Blob uploaded
+                // Make a request for analysis
+                // (...)
+
+            } else {
+              console.log(error);
+            }
+          });
+        } else {
+          console.log(error);
+        }
+      });
+    }
+  });
 
   form.parse(req);
   res.send('OK');
