ea09c1da93442e783d2e33861c3d04df2cd0688d
Maciej Kumorek <mkumorek@gmail.com>
2014-11-21 17:02:31 +0000

    Upload to azure

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,6 +1,10 @@
 var express = require('express'),
   router = express.Router(),
-  Article = require('../models/article');
+  Article = require('../models/article'),
+  formidable = require('formidable'),
+  azure = require('azure-storage'),
+  uuid = require('node-uuid'),
+  entityGen = azure.TableUtilities.entityGenerator;
 
 module.exports = function (app) {
   app.use('/', router);
@@ -18,9 +22,27 @@ router.get('/', function (req, res, next) {
 
 router.post('/upload', function (req, res) {
 
-  var articles = [new Article(), new Article()];
-    res.render('index', {
-      title: 'Welcome to Mosaic Creator',
-      articles: articles
+  var blobSvc = azure.createBlobService();
+  var form = new formidable.IncomingForm();
+  res.respond("OK");
+
+  form.onPart = function(part){
+    blobSvc.createContainerIfNotExists('imagecontainer', function(error, result, response){
+      if(!error){
+
+        blobSvc.createBlockBlobFromStream('imagecontainer', 'image1', part, function(error, result, response){
+          if(!error){
+              // Blob uploaded
+          } else {
+            console.log(error);
+          }
+        });
+      } else {
+        console.log(error);
+      }
     });
+  }
+
+  form.parse(req);
+  res.send('OK');
 });
