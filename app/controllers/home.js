bc116e082707620f522d15c9e988239b7730ea83
Maciej Kumorek <mkumorek@gmail.com>
2014-12-30 13:45:18 +0000

    Added basic oauth

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -4,7 +4,19 @@ var express = require('express'),
   multiparty = require('multiparty'),
   azure = require('azure-storage'),
   uuid = require('node-uuid'),
-  entityGen = azure.TableUtilities.entityGenerator;
+  google = require('googleapis'),
+  entityGen = azure.TableUtilities.entityGenerator,
+  OAuth2Client = google.auth.OAuth2,
+  plus = google.plus('v1');
+
+// Client ID and client secret are available at
+// https://code.google.com/apis/console
+var CLIENT_ID = process.env.CLIENT_ID;
+var CLIENT_SECRET = process.env.CLIENT_SECRET;
+var REDIRECT_URL = process.env.REDIRECT_URI;
+
+// Oauth 2 client
+var oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
 
 module.exports = function (app) {
   app.use('/', router);
@@ -17,6 +29,20 @@ router.get('/', function (req, res, next) {
   var photos = [];
   var blobSvc = azure.createBlobService();
 
+  
+  // generate a url that asks permissions for Google+ and Google Calendar scopes
+  var scopes = [
+    'https://www.googleapis.com/auth/plus.me',
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile'
+  ];
+
+  var url = oauth2Client.generateAuthUrl({
+    access_type: 'offline', // 'online' (default) or 'offline' (gets refresh_token)
+    scope: scopes // If you only need one scope you can pass it as string
+  });
+
+
   blobSvc.listBlobsSegmented('imagecontainer', null, function(error, result, response){
     if(!error){
       // result contains the entries
@@ -28,7 +54,8 @@ router.get('/', function (req, res, next) {
       res.render('index', {
         title: 'Welcome to Mosaic Creator',
         articles: articles,
-        images: photos
+        images: photos,
+        authUrl: url
       });
 
     } else {
@@ -37,13 +64,14 @@ router.get('/', function (req, res, next) {
       res.render('index', {
         title: 'Welcome to Mosaic Creator',
         articles: articles,
-        images: []
+        images: [],
+        authUrl: url
       });
     }
   });
 });
 
-
+// Upload images to the blob
 router.post('/upload', function (req, res) {
 
   var blobSvc = azure.createBlobService();
@@ -102,4 +130,23 @@ router.post('/upload', function (req, res) {
 
   form.parse(req);
   res.send('OK.. uploading');
+});
+
+router.get("/login", function(req,res) {
+  
+
+});
+
+// Google OAuth 2 callback
+router.get("/oauth2callback", function(req, res) {
+  var code = req.query.code;
+
+  oauth2Client.getToken(code, function(err, tokens) {
+    // Now tokens contains an access_token and an optional refresh_token. Save them.
+    if(!err) {
+      console.log(tokens);
+      oauth2Client.setCredentials(tokens);
+      res.redirect("/");
+    }
+  });
 });
